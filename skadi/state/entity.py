f3eb3a854f00669f75c372c752ab85008cc21c36
Joshua Morris <joshua.a.morris@gmail.com>
2013-07-10 21:58:50 -0700

    entity state class

---
+++ skadi/state/entity.py
@@ -0,0 +1,21 @@
+class Entity(object):
+  def __init__(_id, recv_table):
+    self.id = _id
+    self.recv_table = recv_table
+    self.reset()
+
+  def reset(self):
+    self._state = {(prop.var_name, None) for prop in self.recv_table.props}
+
+  def summarize(self):
+    return self._state.copy()
+
+  def __getattr__(self, name):
+    if name in self._state:
+      return self._state[name]
+    return super(Entity, self).__getattr__(name)
+
+  def __setattr__(self, name, value):
+    if name in self._state:
+      self._state[name] = value
+    super(Entity, self).__setattr__(name, value)
