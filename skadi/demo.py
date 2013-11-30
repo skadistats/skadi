638964e19909bf9db5a42db375ab2cecc1f9f260
Joshua Morris <joshua.a.morris@gmail.com>
2013-11-30 04:24:38 -0800

    preliminary documentation for skadi.demo

--- skadi/demo.py
+++ skadi/demo.py
@@ -7,6 +7,21 @@ from skadi.state.util import Snapshot
 
 
 def preroll(io_demo, tick):
+    """
+    Read from a DemoIO (skadi.io.demo) up to the listed tick, gathering first
+    all entries of type CDemoFullPacket (protobuf.impl.demo_pb2). After the
+    last full packet, gather all entries of type CDemoPacket
+    (protobuf.impl.demo_pb2).
+
+    An entry is simply a (peek, message) tuple yielded by DemoIO iteration.
+
+    Arguments:
+    io_demo -- DemoIO over which to iterate (peek, message) entries
+    tick -- tick (in replay time, not game time) to read up to
+
+    Returns a tuple containing two entry lists: full packets, then packets.
+
+    """
     entries = scan_until(io_demo, tick)
 
     full_packets = []
@@ -24,6 +39,23 @@ def preroll(io_demo, tick):
 
 
 def play(io_demo, match):
+    """
+    Iterates over the provided DemoIO (skadi.io.demo), updating the provided
+    Match (skadi.state.match) with data from each CDemoPacket
+    (protobuf.impl.demo_pb2) entry. This update behavior is encapsulated in
+    the Match object.
+
+    Yields the Match snapshots (a python deque collection) at each tick. A
+    Snapshot (skadi.state.util) is a python namedtuple with world state
+    accessible by named attributes.
+
+    Full packet entries encountered during iteration are skipped.
+
+    Arguments:
+    io_demo -- DemoIO over which to iterate (peek, message) entries
+    match -- Match instance to update at each tick
+
+    """
     for peek, message in io_demo:
         if peek.kind == pb_d.DEM_FullPacket:
             continue
@@ -39,6 +71,16 @@ def play(io_demo, match):
 
 
 def scan_kind(io_demo, kind):
+    """
+    Searches DemoIO (skadi.io.demo) for first (peek, message) matching kind.
+
+    Arguments:
+    io_demo -- DemoIO over which to iterate (peek, message) entries
+    kind -- int from EDemoCommands (protobuf.def.demo_pb2) specifying kind
+
+    Returns a (peek, message) tuple, or (None, None) if no match found.
+
+    """
     gen = ((p, m) for p, m in io_demo if p.kind == kind)
 
     try:
@@ -50,6 +92,16 @@ def scan_kind(io_demo, kind):
 
 
 def scan_tick(io_demo, tick):
+    """
+    Searches DemoIO (skadi.io.demo) for first (peek, message) matching tick.
+
+    Arguments:
+    io_demo -- DemoIO over which to iterate (peek, message) entries
+    tick -- int tick value
+
+    Returns a (peek, message) tuple, or (None, None) if no match found.
+
+    """
     gen = ((p, m) for p, m in io_demo if p.tick >= tick)
 
     try:
@@ -61,6 +113,24 @@ def scan_tick(io_demo, tick):
 
 
 def scan_since(io_demo, epoch, fn_eligible=None, fn_complete=None):
+    """
+    Searches DemoIO (skadi.io.demo) for all (peek, message) entries "since"
+    epoch (a tick). Allows a lambda eligibility test, and a lambda completion
+    test.
+
+    Note: This method depends on the underlying IO object's state.
+
+    Arguments:
+    io_demo -- DemoIO over which to iterate (peek, message) entries
+    epoch -- int tick value to scan "since" (everything prior ignored)
+    fn_eligible -- lambda with one argument testing if current entry valid
+    fn_complete -- lambda with one argument allowing short-circuit of search
+
+    Returns list of entries "since" epoch passing "eligible" test, and only
+    until "complete" test returns True.
+
+    """
+    gen = ((p, m) for p, m in io_demo if p.tick >= tick)
     def fn_timely(entry):
         peek, _ = entry
         return peek.tick >= epoch
@@ -69,6 +139,21 @@ def scan_since(io_demo, epoch, fn_eligible=None, fn_complete=None):
 
 
 def scan_until(io_demo, epoch, fn_eligible=None):
+    """
+    Searches DemoIO (skadi.io.demo) for all (peek, message) entries from
+    current position while peek.tick < epoch. Allows a lambda eligibility
+    test.
+
+    Note: This method depends on the underlying IO object's state.
+
+    Arguments:
+    io_demo -- DemoIO over which to iterate (peek, message) entries
+    epoch -- int tick value to scan "since" (everything prior ignored)
+    fn_eligible -- lambda with one argument testing if current entry valid
+
+    Returns a list of entries "until" epoch which also pass "eligible" test.
+
+    """
     def fn_timely(entry):
         peek, _ = entry
         return peek.tick <= epoch
@@ -81,6 +166,22 @@ def scan_until(io_demo, epoch, fn_eligible=None):
 
 
 def scan_between(io_demo, since, until, fn_eligible=None):
+    """
+    Searches DemoIO (skadi.io.demo) for all (peek, message) entries from
+    current position where since < peek.tick < until. Also allows a lambda
+    eligibility test.
+
+    Note: This method depends on the underlying IO object's state.
+
+    Arguments:
+    io_demo -- DemoIO over which to iterate (peek, message) entries
+    epoch -- int tick value to scan "since" (everything prior ignored)
+    fn_eligible -- lambda with one argument testing if current entry valid
+
+    Returns all entries where since < peek.tick < until which also pass
+    "eligible" test.
+
+    """
     def fn_timely(entry):
         peek, _ = entry
         return since <= peek.tick <= until
@@ -93,6 +194,23 @@ def scan_between(io_demo, since, until, fn_eligible=None):
 
 
 def scan_timely(io_demo, fn_timely, fn_eligible, fn_complete):
+    """
+    Searches DemoIO (skadi.io.demo) for all (peek, message) entries from
+    current position while the three functions return correct values.
+
+    Note: This method depends on the underlying IO object's state.
+
+    Arguments:
+    io_demo -- DemoIO over which to iterate (peek, message) entries
+    epoch -- int tick value to scan "since" (everything prior ignored)
+    fn_timely -- lambda with one argument testing if current entry "timely"
+    fn_eligible -- lambda with one argument testing if current entry valid
+    fn_complete -- lambda with one argument allowing short-circuit of search
+
+    Returns all entries where fn_timely is True, fn_eligible is True, and
+    fn_complete is False.
+
+    """
     matches = []
     complete = False
 
